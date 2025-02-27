e71bdd8213e840008ff2016226889ebe9db71a80
rgaud4299 <160020283+rgaud4299@users.noreply.github.com>
2025-02-27 19:50:25 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -6,6 +6,7 @@ const { listingSchema, reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const review = require("../models/review.js");
+const { isLoggedIn } = require("../middleware.js");
 
 const validateReview = (req, res, next) => {
   // console.log(reviewSchema.validate(req.body))
@@ -20,10 +21,14 @@ const validateReview = (req, res, next) => {
 
 // reviw post routes
 
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
+  newReview.author=req.user._id;
   listing.reviews.push(newReview);
+
+  
+    
      
      
      
