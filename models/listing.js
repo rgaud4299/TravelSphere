4c4c81a7ab5b455508c4cc3949c0684253a72e11
Rahul Gaur <160020283+rgaud4299@users.noreply.github.com>
2025-03-02 15:34:13 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,38 +1,82 @@
 
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const Review=require("./review.js")
+
+// const listingSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   }, 
+//   description: String,
+//   image: {
+//     url: String,
+//    filename:String
+//   },
+//   price: Number,
+//   location: String,
+//   country: String,
+//   reviews:[
+//     {
+//       type:Schema.Types.ObjectId,
+//       ref:"Review"
+//     }
+//   ],
+  
+//   owner: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User', 
+// },
+// });
+//   // delete review 
+// listingSchema.post("findOneAndDelete",async(listing)=>{
+// if(listing){
+//   await Review.deleteMany({_id:{$in: listing.reviews}})
+// }
+// });
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review=require("./review.js")
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  }, 
-  description: String,
-  image: {
-    url: String,
-   filename:String
-  },
-  price: Number,
-  location: String,
-  country: String,
-  reviews:[
-    {
-      type:Schema.Types.ObjectId,
-      ref:"Review"
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        url: String,
+        filename: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    contactNumber: { // Added contact number property
+        type: String,
+        required: true, // You can adjust this to false if contact number is optional
+    },
+});
+
+// delete review
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
     }
-  ],
-  
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
-},
 });
-  // delete review 
-listingSchema.post("findOneAndDelete",async(listing)=>{
-if(listing){
-  await Review.deleteMany({_id:{$in: listing.reviews}})
-}
-});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
