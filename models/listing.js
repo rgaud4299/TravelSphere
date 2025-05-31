4d63af15d3391ee460860e3c66cad3583329b9c0
Rahul Gaur <160020283+rgaud4299@users.noreply.github.com>
2025-05-31 18:25:04 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -66,7 +66,7 @@ const listingSchema = new Schema({
         ref: 'User',
     },
     contactNumber: { // Added contact number property
-        type: Number,
+        type: String,
         required: true, // You can adjust this to false if contact number is optional
     },
 });
